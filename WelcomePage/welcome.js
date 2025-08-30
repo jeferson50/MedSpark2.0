f272a48a761dd083773ca892081ec08128b7472a
Jeferson Ramos <jeferson.ramos2026@hilasal.superate.org.sv>
2025-08-29 22:07:38 -0600

    new js

--- WelcomePage/welcome.js
+++ WelcomePage/welcome.js
@@ -6,11 +6,13 @@ if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark", "bg-gray-900", "text-gray-100");
 }
 
+
 darkModeToggle.addEventListener("click", () => {
   body.classList.toggle("dark");
   body.classList.toggle("bg-gray-900");
   body.classList.toggle("text-gray-100");
 
+  
 
   if (body.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
