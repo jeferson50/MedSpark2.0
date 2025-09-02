8016dc110d6f4c22c2882c4502a3e734340d8509
Jeferson Ramos <jeferson.ramos2026@hilasal.superate.org.sv>
2025-09-01 18:31:46 -0600

    Improve Welcome

--- WelcomePage/welcome.js
+++ WelcomePage/welcome.js
@@ -1,18 +1,15 @@
-
 const darkModeToggle = document.getElementById("darkModeToggle");
 const body = document.body;
 
+// Configura el tema según localStorage
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark", "bg-gray-900", "text-gray-100");
 }
-
 
 darkModeToggle.addEventListener("click", () => {
   body.classList.toggle("dark");
   body.classList.toggle("bg-gray-900");
   body.classList.toggle("text-gray-100");
-
-  
 
   if (body.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
@@ -21,8 +18,7 @@ darkModeToggle.addEventListener("click", () => {
   }
 });
 
-
-
+// Función del loading
 function startLoading() {
   const loadingScreen = document.getElementById("loading-screen");
   loadingScreen.classList.remove("hidden");
@@ -31,4 +27,3 @@ function startLoading() {
     window.location.href = "choose-signup.html";
   }, 3000);
 }
-
